ce00a6a696f44f475c9d70c22af48da1aadc72cc
sneha1602 <sneha1602@gmail.com>
2017-12-11 02:23:34 +0100

    Added rules

---
+++ AdaptationRules/src/org/emoflon/ibex/tgg/run/adaptationrules/CC_App.java
@@ -0,0 +1,40 @@
+package org.emoflon.ibex.tgg.run.adaptationrules;
+
+import java.io.IOException;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.commons.lang3.NotImplementedException;
+
+import org.emoflon.ibex.tgg.operational.strategies.cc.CC;
+import org.emoflon.ibex.tgg.runtime.engine.DemoclesEngine;
+
+public class CC_App extends CC {
+
+	public CC_App(String projectName, String workspacePath, boolean debug) throws IOException {
+		super(projectName, workspacePath, debug);
+		registerPatternMatchingEngine(new DemoclesEngine());
+	}
+
+	public static void main(String[] args) throws IOException {
+		BasicConfigurator.configure();
+
+		CC_App cc = new CC_App("AdaptationRules", "./../", false);
+		
+		logger.info("Starting CC");
+		long tic = System.currentTimeMillis();
+		cc.run();
+		long toc = System.currentTimeMillis();
+		logger.info("Completed CC in: " + (toc - tic) + " ms");
+		
+		cc.saveModels();
+		cc.terminate();
+	}
+
+	protected void registerUserMetamodels() throws IOException {
+		// Load and register source and target metamodels
+		throw new NotImplementedException("Please check that your source and target metamodels are loaded and registered.");
+		
+		// Register correspondence metamodel last
+		loadAndRegisterMetamodel(projectPath + "/model/" + projectPath + ".ecore");
+	}
+}
---
+++ AdaptationRules/src/org/emoflon/ibex/tgg/run/adaptationrules/SYNC_App.java
@@ -0,0 +1,40 @@
+package org.emoflon.ibex.tgg.run.adaptationrules;
+
+import java.io.IOException;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.commons.lang3.NotImplementedException;
+
+import org.emoflon.ibex.tgg.operational.strategies.sync.SYNC;
+import org.emoflon.ibex.tgg.runtime.engine.DemoclesEngine;
+
+public class SYNC_App extends SYNC {
+
+	public SYNC_App(String projectName, String workspacePath, boolean debug) throws IOException {
+		super(projectName, workspacePath, debug);
+		registerPatternMatchingEngine(new DemoclesEngine());
+	}
+
+	public static void main(String[] args) throws IOException {
+		BasicConfigurator.configure();
+
+		SYNC_App sync = new SYNC_App("AdaptationRules", "./../", false);
+		
+		logger.info("Starting SYNC");
+		long tic = System.currentTimeMillis();
+		sync.forward();
+		long toc = System.currentTimeMillis();
+		logger.info("Completed SYNC in: " + (toc - tic) + " ms");
+		
+		sync.saveModels();
+		sync.terminate();
+	}
+
+	protected void registerUserMetamodels() throws IOException {
+		// Load and register source and target metamodels
+		throw new NotImplementedException("Please check that your source and target metamodels are loaded and registered.");
+		
+		// Register correspondence metamodel last
+		loadAndRegisterMetamodel(projectPath + "/model/" + projectPath + ".ecore");
+	}
+}
